d8cacc288080d43278d6de587c022505e9160f93
JC <JC@LAPTOP-AJ3R44PU>
2018-03-01 12:10:24 -0600

    Update to .NET Standard 2.0.  Ad  new Xunit Test Lib.

---
+++ src/TestLightContainer/Core/IocContainerTest.cs
@@ -0,0 +1,16 @@
+﻿using LightContainer.Core;
+using Xunit;
+
+namespace TestLightContainer.Core
+{
+    public class IocContainerTest
+    {
+        [Fact]
+        public void Register_and_Resolve_Instance()
+        {
+            var container = new IocContainer();
+
+            //container.RegisterInstance<ITestInstance>()
+        }
+    }
+}
